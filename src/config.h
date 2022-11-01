4d9b424ef6a69db254686ca87b060bb28e03dd53
Igor Maliy <imaliy.filsh@gmail.com>
2022-11-02 01:17:43 +0200

    Update config.h

--- src/config.h
+++ src/config.h
@@ -1,19 +1,19 @@
 #define PIN_ALARM_BUZZER 2
-#define PIN_ALARM_LED 3
-#define PIN_STATE_LED 4
-#define PIN_RELAY 5
-
-#define PIN_SENSOR_LED_1 6
-#define PIN_SENSOR_LED_2 7
-#define PIN_SENSOR_LED_3 8
-#define PIN_SENSOR_LED_4 9
-
-#define PIN_BUTTON A0
+#define PIN_ALARM_LED 4
+#define PIN_STATE_LED 3
+#define PIN_RELAY 9
 
 #define PIN_SENSOR_1 A1
 #define PIN_SENSOR_2 A2
 #define PIN_SENSOR_3 A3
 #define PIN_SENSOR_4 A4
+
+#define PIN_SENSOR_LED_1 8
+#define PIN_SENSOR_LED_2 7
+#define PIN_SENSOR_LED_3 6
+#define PIN_SENSOR_LED_4 5
+
+#define PIN_BUTTON A0
 
 #define FLOOD_LEVEL 900
 #define RELAY_DURATION 5000
